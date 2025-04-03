89d720cecf3c72b3db07008419bc283913703936
junaiddshaukat <junaiddshaukat@gmail.com>
2025-04-03 17:40:06 +0500

    refrences start

---
+++ ch-04/refrences_and_borrowing/target/debug/deps/refrences_and_borrowin-0645a96297f7cddf.d
@@ -0,0 +1,5 @@
+D:\Coding\learn-rust\ch-04\refrences_and_borrowing\target\debug\deps\librefrences_and_borrowin-0645a96297f7cddf.rmeta: src\main.rs
+
+D:\Coding\learn-rust\ch-04\refrences_and_borrowing\target\debug\deps\refrences_and_borrowin-0645a96297f7cddf.d: src\main.rs
+
+src\main.rs:
---
+++ ch-04/refrences_and_borrowing/target/debug/deps/refrences_and_borrowin-fff4ecbf377e70f7.d
@@ -0,0 +1,5 @@
+D:\Coding\learn-rust\ch-04\refrences_and_borrowing\target\debug\deps\librefrences_and_borrowin-fff4ecbf377e70f7.rmeta: src\main.rs
+
+D:\Coding\learn-rust\ch-04\refrences_and_borrowing\target\debug\deps\refrences_and_borrowin-fff4ecbf377e70f7.d: src\main.rs
+
+src\main.rs:
